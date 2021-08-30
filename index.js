6879308e970731bba590b7a44b2bfdcb6b020b66
Atheelah <atheelahvanderschyff17@gmail.com>
2021-08-30 23:09:48 +0200

    javascript added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function openNav(){
-    
+function openNav() {
+  document.getElementById("side-bar").style.width = "500px";
+  document.getElementById("toggle").style.position = "static";
+  console.log(openNav);
+}
+function closeNav() {
+  document.getElementById("side-bar").style.width = "0";
+  document.getElementById("toggle").style.marginRight = "0";
+  document.getElementById("toggle").style.position = "relative";
 }
