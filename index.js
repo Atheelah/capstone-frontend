dec200d5a2efeedef449eefefea56ef7f50dd95e
Atheelah <atheelahvanderschyff17@gmail.com>
2021-08-31 16:11:49 +0200

    updated

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-function openNav() {
-  document.getElementById("side-bar").style.width = "500px";
-  document.getElementById("toggle").style.position = "static";
-  console.log(openNav);
+function toggleNav() {
+  document.querySelector('.side-nav').classList.toggle('active')
+  document.querySelector(".blur").classList.toggle("blurr")
 }
-function closeNav() {
-  document.getElementById("side-bar").style.width = "0";
-  document.getElementById("toggle").style.marginRight = "0";
-  document.getElementById("toggle").style.position = "relative";
-}
+
+document.getElementById('toggle').addEventListener('click', () => {
+  toggleNav()
+})
+
+document.querySelector('.closebtn').addEventListener('click', () => {
+  toggleNav()
+})
