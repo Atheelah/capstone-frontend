4c841039e247c5dac9cf584be8ab520385a8a6bf
Atheelah <atheelahvanderschyff17@gmail.com>
2021-09-06 16:05:00 +0200

    completed registration and login

--- signin.js
+++ signin.js
@@ -1,52 +1,10 @@
-// fetch("https://quiet-lowlands-03571.herokuapp.com/")
-// .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
-//   let signinBtn = document.querySelector(".sign-in-btn").addEventListener("click",
-//   function signIn() {
-//     let username = document.querySelector("#username").value;
-//     let password = document.querySelector("#password").value;
-//     console.log(username,password);
-// }
-//   );
-
-//   fetch("https://quiet-lowlands-03571.herokuapp.com/", {
-//     method: "PATCH",
-//     body: JSON.stringify({
-//       username,
-//       password,
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//     },
-//   })
-//   .then((res) => res.json())
-//   .then((res) => {
-//     console.log(res);
-//     if (!res.data) {
-//       document.querySelector("#error").innerHTML =
-//         "Please register";
-//       return;
-//     } else {
-//       localStorage.setItem("user", JSON.stringify(res.data));
-//       window.location = "./index.html";
-//     }
-//   });
-
-// fetch("https://quiet-lowlands-03571.herokuapp.com")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
-
+// function to login
 function login() {
   // GETTING DATA FROM FORM
   let username = document.querySelector("#username").value;
   let password = document.querySelector("#password").value;
   console.log(username, password);
-
+  // api created in the back
   fetch("https://quiet-lowlands-03571.herokuapp.com/login/", {
     method: "PATCH",
     body: JSON.stringify({
@@ -61,11 +19,57 @@ function login() {
     .then((res) => {
       console.log(res);
       if (!res.data) {
-        document.querySelector("#error").innerHTML = "Please register";
+        alert("User not registered!");
         return;
       } else {
         localStorage.setItem("user", JSON.stringify(res.data));
         window.location = "./index.html";
+      }
+    });
+}
+var loginButton = document.getElementById("loginButton");
+var registerButton = document.getElementById("registerButton");
+
+loginButton.onclick = function () {
+  document.querySelector("#flipper").classList.toggle("flip");
+};
+
+registerButton.onclick = function () {
+  document.querySelector("#flipper").classList.toggle("flip");
+};
+
+// heroku link --> ("https://quiet-lowlands-03571.herokuapp.com")
+
+function registration() {
+  let firstName = document.querySelector("#first-name").value;
+  let lastName = document.querySelector("#last-name").value;
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password ").value;
+  let email = document.querySelector("#email").value;
+  console.log(firstName, lastName, username, password, email);
+
+  fetch("https://quiet-lowlands-03571.herokuapp.com/registration/", {
+    method: "POST",
+    body: JSON.stringify({
+      firstName,
+      lastName,
+      username,
+      password,
+      email,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res.status_code == 201);
+      {
+        document.querySelector("#error").innerHTML =
+          "You've been registered successfully";
+        setTimeout(function () {
+          window.location = "./sign in.html";
+        }, 3000);
       }
     });
 }
