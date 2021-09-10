1fa7bf35d4d2eef8baf97f4f3057ee7d154362a4
Atheelah <atheelahvanderschyff17@gmail.com>
2021-09-10 08:59:10 +0200

    updated

--- add.js
+++ add.js
@@ -5,31 +5,31 @@ function addBook() {
   let description = document.querySelector(".description").value;
   let genre = document.querySelector(".genre").value;
   let price = document.querySelector(".price").value;
+
+  fetch("https://end-of-term.herokuapp.com/add-book/", {
+    method: "POST",
+    body: JSON.stringify({
+      image: imageUrl,
+      book_title: bookName,
+      author: author,
+      description: description,
+      category: genre,
+      price: price,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorisation: `jwt ${token}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+
+      if (res.status_code == 201) {
+        window.location.reload();
+      }
+    });
 }
-
-fetch("https://end-of-term.herokuapp.com/add-book/", {
-  method: "POST",
-  body: JSON.stringify({
-    imgUrl,
-    bookName,
-    author,
-    description,
-    genre,
-    price,
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8",
-    // "Authorisation": `jwt ${token}`
-  },
-})
-  .then((res) => res.json())
-  .then((res) => {
-    console.log(res);
-
-    if (res.status_code == 201) {
-      window.location.reload();
-    }
-  });
 
 let addForm = document.querySelector(".adding");
 addForm.addEventListener("submit", (e) => {
